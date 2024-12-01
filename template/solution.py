25c021fd95c405834e3f58ca79871426e8bed07f
Lorenz Gaertner <lorenz.gaertner@physik.uni-muenchen.de>
2024-12-01 14:17:25 +0100

    improve loading via numpy

--- template/solution.py
+++ template/solution.py
@@ -5,10 +5,10 @@ def solution(data):
 
 if __name__=='__main__':
     with open('test.txt', 'r') as f:
-        input_test = f.read()
+        input_test = np.loadtxt(f)
 
     with open('input.txt', 'r') as f:
-        input = f.read()
+        input = np.loadtxt(f)
 
     data = input if input else input_test
 
